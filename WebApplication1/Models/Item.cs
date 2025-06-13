bc6bd215f6c3d5dd22cd0478f931e99caa413654
S0u1y <filda.kubik@seznam.cz>
2025-06-13 11:15:18 +0200

    Add Item

---
+++ WebApplication1/Models/Item.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace WebApplication1.Models{
+    public class Item
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        // public string Description { get; set; }
+        // public int Price { get; set; }
+    }
+}
+
