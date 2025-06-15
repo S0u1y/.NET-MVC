4b4d29820de93a0bd7d1c724830c41424243a427
S0u1y <filda.kubik@seznam.cz>
2025-06-15 22:13:15 +0200

    update item with ORM

--- WebApplication1/Models/Item.cs
+++ WebApplication1/Models/Item.cs
@@ -1,9 +1,14 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApplication1.Models{
     public class Item
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
+        [Required]
         public string Name { get; set; }
         // public string Description { get; set; }
         // public int Price { get; set; }
