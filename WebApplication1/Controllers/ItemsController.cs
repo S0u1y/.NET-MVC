0df212d639102b4293c6027e29b23d0b7e343c4b
S0u1y <filda.kubik@seznam.cz>
2025-06-17 08:01:09 +0200

    update for CRUD

--- WebApplication1/Controllers/ItemsController.cs
+++ WebApplication1/Controllers/ItemsController.cs
@@ -1,9 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Controllers.Data;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers{
     public class ItemsController : Controller
     {
+        private ApplicationContext context;
+        public ItemsController(ApplicationContext _context)
+        {
+            context = _context;
+        }
+
         public IActionResult Overview()
         {
             Item item = new Item { Name = "Keyboard" };
@@ -11,6 +18,13 @@ namespace WebApplication1.Controllers{
             return View(item);
         }
 
+        public IActionResult Index()
+        {
+            var items = context.Items.ToList();
+
+            return View(items);
+        }
+        
         public IActionResult Edit(int id)
         {
             return Content("id = " + id);
